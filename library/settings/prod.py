3fac65ac5f6a78727a9bfc79994b059b17b4a74d
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-04-15 19:00:28 +0100

    Updated

--- library/settings/prod.py
+++ library/settings/prod.py
@@ -1,3 +1,26 @@
-from base import *
+from .base import *
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
 
 DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+
+
+DATABASES = {'default': dj_database_url.config()}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
