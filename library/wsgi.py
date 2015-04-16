090ef95ee5dfa6e63476c262a6b5c77f5ae14d80
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-04-16 11:23:15 +0100

    added whitnoise to wsgi

--- library/wsgi.py
+++ library/wsgi.py
@@ -11,6 +11,7 @@ import os
 import envvars
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 # os.environ.load()
@@ -18,3 +19,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", envvars.get("DJANGO_SETTINGS_MOD
 
 application = get_wsgi_application()
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
