409f545d38a3180727bd45420256c49c0d430807
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-04-16 11:08:59 +0100

    Adjust

--- library/settings/prod.py
+++ library/settings/prod.py
@@ -9,7 +9,6 @@ TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-# STATIC_ROOT = 'staticfiles'
 
 
 
@@ -17,3 +16,5 @@ DATABASES = {'default': dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
