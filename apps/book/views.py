be807c04992ce249c93a6208743c2614153f4dbf
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-04-20 11:44:37 +0100

    Updated views to edit a book

--- apps/book/views.py
+++ apps/book/views.py
@@ -9,8 +9,10 @@ from django.core.context_processors import csrf
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import auth
 from django.template import RequestContext
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
-from apps.book.models import BookLease
+from apps.book.models import BookLease, Book
 from apps.libraryuser.models import Fellow
 from apps.book.forms import AddForm, LendBookForm
 
@@ -31,40 +33,72 @@ class BookListView(ListView):
 
 class BookDetailView(DetailView):
 
-    model = BookLease
-
-    def dispatch(self, *args, **kwargs):
-        return super(BookDetailView, self).dispatch(*args, **kwargs)
+    model = Book
 
     def get_context_data(self, **kwargs):
         context = super(BookDetailView, self).get_context_data(**kwargs)
+        print context
         return context
 
+# @login_required
+# def get_book(request):
+#     # if this is a POST request we need to process the form data
+#     user = request.user
+#     print user
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = AddForm(request.POST)
+#         print form
+#         # check whether it's valid:
+#         if form.is_valid():
+#             form.save()
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return render(request, "thank_you.html", locals())
 
-@login_required
-def get_book(request):
-    # if this is a POST request we need to process the form data
+
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = AddForm()
+#         # alert('Please fill all fields')
+
+#     return render(request, 'book_new.html', {'form': form})
+
+
+
+def get_book(request, id=None, template_name = 'book_new.html' ):
     user = request.user
-    print user
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
+
+    if id:
+        book = get_object_or_404 (Book, pk=id)
+
+    if request.POST:
+
+        # book = get_object_or_404(Book, id=request.POST.get('id'))
+
         form = AddForm(request.POST)
-        print form
-        # check whether it's valid:
+    # check whether form is valid
+
         if form.is_valid():
             form.save()
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return render(request, "thank_you.html", locals())
 
+        return render(request, "thank_you.html", locals())
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = AddForm()
-        # alert('Please fill all fields')
 
-    return render(request, 'book_new.html', {'form': form})
+    return render_to_response (template_name, {'form': form,},context_instance = RequestContext(request))
+
+
+#     context = {'book_form': book_form, 'book': book}
+#     return render(request, 'borrow_book.html', context)
+
+# def get(self, request):
+
+#     book_form = AddForm()
+#     context = {'book_form': book_form}
+#     return render(request, 'book_new.html', context)
 
 
 @login_required
