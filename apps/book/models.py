2b8eac1b74273aedc1a6fb02713af53ea3d58b12
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-04-22 09:22:49 +0100

    Removed redundate codes

--- apps/book/models.py
+++ apps/book/models.py
@@ -33,64 +33,8 @@ class BookLease(models.Model):
     due_date = models.DateField(null=True)
     returned = models.NullBooleanField()
 
-    # def get_num_available_book(self):
-
-        # book = Book.objects.get(id=id)
-    #     total_leased = self.book_leases.all().count()
-    #     available = self.quantity - total_leased
-    #     return available
-    
-    # num_book_available = property(get_num_available_book)
 
     def __unicode__(self): 
         return '{}'.format(self.book)
 
-    # def get_num_available_book(self, filter=None):
-    #     available = 0
-
-
-
-    # def get_num_available(self, filter=None):
-    #     available = 0
-    #     obj = self.booklease.all()
-    #     if filter==None:
-    #         for book in obj:
-    #             num_available = 
-    #          def get_amount_paid(self, filter=None):
-    #     amount_paid = 0
-    #     obj = self.payment_histories.all()
-    #     if filter==None:
-    #         if obj:
-    #             for item in obj:
-    #                 amount_paid = amount_paid + item.sum_paid
-    #                 continue
-    #         else:
-    #             amount_paid = '--'
-    #     else:
-    #         try:
-    #             current_obj = self.payment_histories.filter(date=filter)
-    #             current_obj_id = current_obj[0].id
-    #             for item in obj:
-    #                 if item.id < current_obj_id:
-    #                     amount_paid = amount_paid + item.sum_paid
-    #                 continue
-    #         except Exception, e:
-    #             amount_paid = '--'
-            
-    #     return amount_paid
-
-    # objects = BookManager()
-
-    # def get_num_borrower(self):
-    #     borrower = self.borrower.all().count()
-    #     return borrower
-
-    # def get_num_available(self):
-    #     available = 0
-    #     for borrower in borrowers:
-    #         if returned == True:
-    #             available = Book.objects.count() + available
-    #         else:
-    #             available = available - Book.objects.count()
-    #     return available
-
+ 
