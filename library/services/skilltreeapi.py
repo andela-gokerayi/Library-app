6b1b59a23f6c547597e03111d40c76be1ad9d370
Gbolahan Okerayi <gbolahan.okerayi@andela.co>
2015-04-30 17:02:36 +0100

    Updated skilltree api code

--- library/services/skilltreeapi.py
+++ library/services/skilltreeapi.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from django.http import HttpResponse
-from library.settings.base import SKILLTREE_API_URL
+from library.settings.base import SKILLTREE_API_URL, SKILLTREE_API_KEY
 from library.core.utils import to_dict
 
 
@@ -9,32 +9,32 @@ class SkillTree():
     """docstring for SkillTree"""
     def __init__(self):
         self.url = SKILLTREE_API_URL
-        # self.headers = {'page': SKILLTREE_API_PAGE}
+        self.headers = {'X-AUTH-TOKEN': SKILLTREE_API_KEY}
 
-    def fetch_data(self, url, **kwargs):
+    def fetch_data(self, url=None, **kwargs):
         ''' Method to fetch data fom skilltree
             :param url: The url to request data from, default is the instance url
             :param kwargs: Any other extra keyword parameters
         '''
         url = url or self.url
-        result = []
+        get_results = []
         page = 1
         while True:
             params = {'page': page}
-            response = requests.get(url, params=params, data=json.dumps(kwargs))
-            if requests_status_code == 404 or not response.json():
+            response = requests.get(url, params=params, data=json.dumps(kwargs), headers=self.headers)
+            if response.status_code == 404 or not response.json():
                 break
-            result.extend(response.json())
+            get_results.extend(response.json())
             page += 1        
-        return result
+        return get_results
 
 
 def get_fellow_info():
     from apps.libraryuser.models import Fellow
-    skill = SkillTree()
-    url = SKILLTREE_API_URL
-    # headers = SKILLTREE_API_PAGE
-    results = SkillTree.fetch_data(skill, url)
+
+    skill = SkillTree() 
+    results = skill.fetch_data()
+    print results
     num_results = len(results)
     print num_results
     for index in xrange(num_results):
