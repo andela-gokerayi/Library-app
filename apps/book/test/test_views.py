5997710245c328671f90ae308c7c946b608b763f
Arinde Eniola <eniola.arinde@andela.co>
2015-06-26 16:33:30 +0100

    test the BookListView class to check if it returns a context when logged in

--- apps/book/test/test_views.py
+++ apps/book/test/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-# from django.test import LiveServerTestCase
+from django.contrib.auth.models import User
 from django.test.client import Client
 from django.core.urlresolvers import reverse
 # from django_dynamic_fixture import G
@@ -12,23 +12,24 @@ from apps.book.models import Book, BookLease
 class BookListViewTests(TestCase):
     """docstring for BookListViewTests"""
     def test_books_in_the_content(self):
-        pass
-        # TODO set up login so this test can run
+        password = 'secret'
+        user = User.objects.create_superuser(username='admin', password=password, email='admin@admin.com')
+    
+        client = Client()
+        logged_in = client.login(username=user.username, password=password)
+        self.assertTrue(logged_in)
+        
+        response = client.get('/home/')
 
-        # client = Client()
-        # response = client.get('/home/')
+        self.assertEquals(list(response.context['object_list']), [])
 
-        # print(response)
-        # print(response.content)
-        # self.assertEquals(list(response.content['object_list']), [])
-
-        # Book.objects.create(title='getting things done',
-        #                     author='david allen', 
-        #                     isbn_number='22434643436',
-        #                     date_recieved='2015-04-16',
-        #                     quantity='3',
-        #                     source='dave',
-        #                     category='professional development'
-        #                     )
-        # response = client.get('/home/')
-        # self.assertEquals(response.context['object_list'].count(), 1)
+        Book.objects.create(title='getting things done',
+                            author='david allen', 
+                            isbn_number='22434643436',
+                            date_recieved='2015-04-16',
+                            quantity='3',
+                            source='dave',
+                            category='professional development'
+                            )
+        response = client.get('/home/')
+        self.assertEquals(response.context['object_list'].count(), 1)
